4db17ed17dd1d34b31aea9cc2e673e8dc71b0abe
Keigo Tanaka <keigo.tanaka@gmail.com>
2013-05-05 13:11:16 +0000

    Added debug mode.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 aws
 ===
+
+- cf-edge-location.rb
+List edge location IP addresses. Usually, this command takes about 10 minutes to complete.
+
+Sample:
+$ ruby cf-edge-location.rb
+{"dfw3"=>"xxx.xxx.xxx.xxx",
+ "sfo9"=>"xxx.xxx.xxx.xxx",
+ "sfo5"=>"xxx.xxx.xxx.xxx",
+ ...
+ "iad5"=>"xxx.xxx.xxx.xxx"}
+
+Try --debug to see what is going on.
+
--- cf-edge-location.rb
+++ cf-edge-location.rb
@@ -7,6 +7,53 @@ require 'mechanize'
 require 'resolv'
 require 'net/ping'
 
+require 'getoptlong'
+
+opts = GetoptLong.new(
+  [ '--help',      '-h', GetoptLong::NO_ARGUMENT ],
+  [ '--debug'   ,  '-d', GetoptLong::NO_ARGUMENT ],
+  [ '--full-scan', '-f', GetoptLong::NO_ARGUMENT ]
+)
+
+$debug = nil
+$full_scan = nil
+opts.each do |opt, arg|
+  case opt
+    when '--help'
+      puts "#{$0} [OPTION]
+
+-h, --help:
+   Show help.
+
+-d, --debug
+   Debug mode.
+
+-f, --full-scan
+   Full scan mode.
+
+"
+      exit
+    when '--debug'
+      $debug = true
+    when '--full-scan'
+      $full_scan = true
+  end
+end
+
+#
+#
+#
+
+def dpp(msg)
+  if $debug
+    if msg.class == String
+      puts msg
+    else
+      pp msg
+    end
+  end
+end
+
 agent = Mechanize.new
 url = "https://forums.aws.amazon.com/ann.jspa?annID=910"
 agent.get(url)
@@ -40,12 +87,37 @@ for cfcidr in cfips do
         edges[edg].push tmpip + "/24"
       end
 
-      #tmpip = cidr4.nth(cnt + 10)
-      #ping_tcp = Net::Ping::TCP.new(tmpip, 80)
-      #puts "#{tmpip}:80\t]" if ping_tcp.ping?
     end
+    # I assume they are using the network block partitioning every 256 addresses.( /24 CIDR )
     cnt = cnt + 256
   end
 end
 
-pp edges
+uphost_on_the_edges = Hash::new
+
+edges.each{|edge, cidrs|
+
+  dpp edge
+  dpp cidrs
+  for cidr in cidrs
+    if ! uphost_on_the_edges[edge].nil?
+      break
+    end
+
+    tmpcidr = NetAddr::CIDR.create(cidr)
+    tmptargets = *(5..250).sort_by{rand}.values_at(0..9)
+    for tmptarget in tmptargets
+      tmpip = tmpcidr.nth(tmptarget)
+      ping_tcp = Net::Ping::TCP.new(tmpip, 80)
+      if ping_tcp.ping?
+        uphost_on_the_edges[edge] = tmpip
+        dpp "#{tmpip}:80 up"
+        break
+      else
+        dpp "#{tmpip}:80 failed.."
+      end
+    end
+  end
+}
+
+pp uphost_on_the_edges
